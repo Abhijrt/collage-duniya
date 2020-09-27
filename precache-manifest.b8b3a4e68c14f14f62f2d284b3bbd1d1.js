b72f9b0d41ca4879290ec0c04f8a061b61375732
Abhay Jirati <abhi.jrt12@gmail.com>
2020-09-27 18:42:01 +0530

    Updates

---
+++ precache-manifest.b8b3a4e68c14f14f62f2d284b3bbd1d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab253a2e37017b3454afa50d785add92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77aa8e7f1043841825c5",
+    "url": "/static/css/main.764dfc73.chunk.css"
+  },
+  {
+    "revision": "709fa8cca7e48af02664",
+    "url": "/static/js/2.510a6528.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.510a6528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77aa8e7f1043841825c5",
+    "url": "/static/js/main.4f241094.chunk.js"
+  },
+  {
+    "revision": "791355fc909f62e7c917",
+    "url": "/static/js/runtime-main.960ada93.js"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/static/media/college_02.619ee97e.jpg"
+  }
+]);
